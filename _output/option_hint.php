ca15ffacb5ff275fb3b85bb4ca7f5f73636d52b5
Simon Hampel <simon@hampelgroup.com>
2025-12-11 15:55:25 +1100

    bump version to 1.1.3

---
+++ _output/option_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property string|null $hampelHideAdvertsDisabledNodes Disabled Nodes
+ * @property string|null $hampelHideAdvertsDisabledUsergroups Disabled Usergroups
+ */
+class Options
+{
+}
